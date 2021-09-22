02411db03b7ac5e7ee9f6894025df93af4ea84de
Vitalii Fedyk <vitalii.fedyk.it.2021@lpnu.ua>
2021-09-22 18:10:26 +0300

    444f

---
+++ lab 3.4.cpp
@@ -0,0 +1,30 @@
+﻿//lab 3.4.cpp
+//Федик Віталій
+//Лабораторна робота №3.4
+//Розгалуження, задане плоскою фігурою
+//Варіант 11
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; //вхідний аргумент
+	double y; //вхідний аргумент
+	double R; //вхідний аргумент
+
+	cout << "x="; cin >> x;
+	cout << "y="; cin >> y;
+	cout << "R="; cin >> R;
+
+	//розгалуження в повній формі
+	if (y <= 0 && y >= -x - R && x <= 0)
+		cout << "Yes" << endl;
+	else
+		if (y <= sqrt(R * R - x * x) && y >= 0 && x <= R && x>=0)
+			cout << "Yes" << endl;
+		else
+			cout << "No" << endl;
+	cin.get();
+	return 0;
+}
